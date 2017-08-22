79718b134de326b318c3dbc3b496e217c9b4ec8a
Carl Quivron <carl.quivron@gmail.com>
2017-08-22 16:30:23 +0200

    Reviews + beginning of wishlists

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,20 +1,23 @@
 class ReviewsController < ApplicationController
 
   def new
+    @place = Place.find(params[:place_id])
     @review = Review.new
+    authorize @review
   end
 
   def create
-    place = Product.find(params[:product_id])
-    review = place.reviews.new(product_params)
+    place = Place.find(params[:place_id])
+    review = place.reviews.new(place_params)
+    authorize review
 
 
     if review.save
       flash[:notice] = "Review added"
-      redirect_to product_path(product)
+      redirect_to place_path(place)
     else
       flash[:alert] = "Can't add the review"
-      redirect_to product_path(product)
+      redirect_to place_path(place)
     end
 
 
@@ -22,11 +25,7 @@ class ReviewsController < ApplicationController
   
   private
 
-  def product_params
-    params.require(:review).permit(
-                                    :content,
-                                    :rating,
-                                    :user_id
-                                    )
+  def place_params
+    params.require(:review).permit(:content,:rating,:user_id)
   end
 end
--- app/models/wishlist.rb
+++ app/models/wishlist.rb
@@ -1,4 +1,5 @@
 class Wishlist < ApplicationRecord
   belongs_to :user
   has_many :wishlists_places
+  validates_uniqueness_of :name, scope: :user_id
 end
