4b4b3fb0543a9e995cccc9db87f7939c8e7446ae
augustin poupard <augustin.poupard@gmail.com>
2017-08-22 11:32:27 +0200

    2 psuh seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,7 @@ Place.create!(
               longitude: " -81.366806",
               description: "Grand Cayman has some of the best kiteboarding conditions in the world. The wind tends to be steady and warm, and the protected bays situated all over the island offer lots of room for experienced riders to cruise around and for beginners to build their skills. If you’re new to the sport or not interested in traveling with all your gear, check out Sweet Spot Watersports for rentals and lesson packages."
               photo: "cayman.jpg"
-              country_name:"Cayman Islands"
+              country:"Cayman Islands"
               )
 
 Place.create!(
@@ -21,18 +21,18 @@ Place.create!(
               longitude: "1.6667",
               description: "This beautiful beach town offers a riding area guarded by reefs, which create relatively shallow and flat waters, and the wind blows at least 20 knots on most days. The reef also means that the riding area is minimally affected by tides, giving you hours of uninterrupted play time."
               photo: "egypt1.jpg"
-              country_name:"Egypt"
+              country:"Egypt"
               )
 
 Place.create!(
               id:"3"
-              city: "Wissant",
-              name: "Wissant",
+              city: "Maui",
+              name: "Maui",
               latitude: "50.8833",
               longitude: "1.6667",
-              description: "Wissant, à la pointe du pas de Calais est le spot le plus venté et fréquenté du nord. Assez accessible par vent modéré, il peut devenir très technique par vent fort avec un courant et une barre dissuasive."
-              photo: "wissant.jpg"
-              country_name:"France"
+              description: "Maui is arguably the best known kiteboarding destination in the world, and Kite Beach is probably the best place to experience all that Maui has to offer. The beautiful tropical beach has clean, warm water, steady trade winds, long stretches of calm water, and gentle waves for more experienced riders. The area is full of world-class instructors and amazing athletes. Want to try kiteboarding? Check out Action Sports Maui for lessons and gear."
+              photo: "hawaii.jpg"
+              country:"Hawaii"
               )
 
 Place.create!(
@@ -43,7 +43,7 @@ Place.create!(
               longitude: "1.6667",
               description: "Wissant, à la pointe du pas de Calais est le spot le plus venté et fréquenté du nord. Assez accessible par vent modéré, il peut devenir très technique par vent fort avec un courant et une barre dissuasive."
               photo: "wissant.jpg"
-              country_name:"France"
+              country:"France"
               )
 
 Place.create!(
@@ -54,7 +54,7 @@ Place.create!(
               longitude: "1.6667",
               description: "Wissant, à la pointe du pas de Calais est le spot le plus venté et fréquenté du nord. Assez accessible par vent modéré, il peut devenir très technique par vent fort avec un courant et une barre dissuasive."
               photo: "wissant.jpg"
-              country_name:"France"
+              country:"France"
               )
 
 Place.create!(
@@ -65,7 +65,7 @@ Place.create!(
               longitude: "1.6667",
               description: "Wissant, à la pointe du pas de Calais est le spot le plus venté et fréquenté du nord. Assez accessible par vent modéré, il peut devenir très technique par vent fort avec un courant et une barre dissuasive."
               photo: "wissant.jpg"
-              country_name:"France"
+              country:"France"
               )
 
 puts ‘Seed Completed’
