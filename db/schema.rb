01a511f1a5a9b4dbd3ae8e88d9612a1fba57a616
Carl Quivron <carl.quivron@gmail.com>
2017-08-22 11:18:46 +0200

    Ajout des premières ressources

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,41 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170821115921) do
+ActiveRecord::Schema.define(version: 20170822085227) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "monthly_ratings", force: :cascade do |t|
+    t.bigint "place_id"
+    t.string "month_number"
+    t.integer "rating"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["place_id"], name: "index_monthly_ratings_on_place_id"
+  end
+
+  create_table "places", force: :cascade do |t|
+    t.string "city"
+    t.string "name"
+    t.float "latitude"
+    t.float "longitude"
+    t.text "description"
+    t.string "country"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "place_id"
+    t.text "content"
+    t.integer "rating"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["place_id"], name: "index_reviews_on_place_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -32,4 +63,27 @@ ActiveRecord::Schema.define(version: 20170821115921) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  create_table "wishlists", force: :cascade do |t|
+    t.bigint "user_id"
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_wishlists_on_user_id"
+  end
+
+  create_table "wishlists_places", force: :cascade do |t|
+    t.bigint "place_id"
+    t.bigint "wishlist_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["place_id"], name: "index_wishlists_places_on_place_id"
+    t.index ["wishlist_id"], name: "index_wishlists_places_on_wishlist_id"
+  end
+
+  add_foreign_key "monthly_ratings", "places"
+  add_foreign_key "reviews", "places"
+  add_foreign_key "reviews", "users"
+  add_foreign_key "wishlists", "users"
+  add_foreign_key "wishlists_places", "places"
+  add_foreign_key "wishlists_places", "wishlists"
 end
