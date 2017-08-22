396cd3dfd79ce6f4685525cca5bb7e7878d6974a
augustin poupard <augustin.poupard@gmail.com>
2017-08-22 11:25:01 +0200

    add 2 place seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,71 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts ‘Erasing all the DB’
+puts ‘Seeding your app’
+
+
+Place.create!(
+              id:"1"
+              city: "Grand Cayman",
+              name: "Grand Cayman",
+              latitude: " 19.292997",
+              longitude: " -81.366806",
+              description: "Grand Cayman has some of the best kiteboarding conditions in the world. The wind tends to be steady and warm, and the protected bays situated all over the island offer lots of room for experienced riders to cruise around and for beginners to build their skills. If you’re new to the sport or not interested in traveling with all your gear, check out Sweet Spot Watersports for rentals and lesson packages."
+              photo: "cayman.jpg"
+              country_name:"Cayman Islands"
+              )
+
+Place.create!(
+              id:"2"
+              city: "Nabq Bay",
+              name: "Nabq Bay",
+              latitude: "50.8833",
+              longitude: "1.6667",
+              description: "This beautiful beach town offers a riding area guarded by reefs, which create relatively shallow and flat waters, and the wind blows at least 20 knots on most days. The reef also means that the riding area is minimally affected by tides, giving you hours of uninterrupted play time."
+              photo: "egypt1.jpg"
+              country_name:"Egypt"
+              )
+
+Place.create!(
+              id:"3"
+              city: "Wissant",
+              name: "Wissant",
+              latitude: "50.8833",
+              longitude: "1.6667",
+              description: "Wissant, à la pointe du pas de Calais est le spot le plus venté et fréquenté du nord. Assez accessible par vent modéré, il peut devenir très technique par vent fort avec un courant et une barre dissuasive."
+              photo: "wissant.jpg"
+              country_name:"France"
+              )
+
+Place.create!(
+              id:"4"
+              city: "Wissant",
+              name: "Wissant",
+              latitude: "50.8833",
+              longitude: "1.6667",
+              description: "Wissant, à la pointe du pas de Calais est le spot le plus venté et fréquenté du nord. Assez accessible par vent modéré, il peut devenir très technique par vent fort avec un courant et une barre dissuasive."
+              photo: "wissant.jpg"
+              country_name:"France"
+              )
+
+Place.create!(
+              id:"5"
+              city: "Wissant",
+              name: "Wissant",
+              latitude: "50.8833",
+              longitude: "1.6667",
+              description: "Wissant, à la pointe du pas de Calais est le spot le plus venté et fréquenté du nord. Assez accessible par vent modéré, il peut devenir très technique par vent fort avec un courant et une barre dissuasive."
+              photo: "wissant.jpg"
+              country_name:"France"
+              )
+
+Place.create!(
+              id:"6"
+              city: "Wissant",
+              name: "Wissant",
+              latitude: "50.8833",
+              longitude: "1.6667",
+              description: "Wissant, à la pointe du pas de Calais est le spot le plus venté et fréquenté du nord. Assez accessible par vent modéré, il peut devenir très technique par vent fort avec un courant et une barre dissuasive."
+              photo: "wissant.jpg"
+              country_name:"France"
+              )
+
+puts ‘Seed Completed’
