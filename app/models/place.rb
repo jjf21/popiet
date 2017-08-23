9ebde6bdbabad4c278ff7c68ab911b8d7abbb7af
Carl Quivron <carl.quivron@gmail.com>
2017-08-23 13:20:47 +0200

    fix address attributes to places

--- app/models/place.rb
+++ app/models/place.rb
@@ -2,6 +2,6 @@ class Place < ApplicationRecord
   has_many :monthly_ratings
   has_many :reviews, dependent: :destroy
   has_many :wishlists_places
-   geocoded_by :address
+  geocoded_by :address
   after_validation :geocode, if: :address_changed?
 end
