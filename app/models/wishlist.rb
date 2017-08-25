855d123975af9c7f2bb666383a33339b987c75f6
Carl Quivron <carl.quivron@gmail.com>
2017-08-25 13:03:12 +0200

    Woaahhh

--- app/models/wishlist.rb
+++ app/models/wishlist.rb
@@ -3,4 +3,14 @@ class Wishlist < ApplicationRecord
   has_many :wishlists_places, dependent: :destroy
   has_many :places, through: :wishlists_places
   validates_uniqueness_of :name, scope: :user_id
+
+  def has?(place_id)
+    value = false
+    self.wishlists_places.each do |wish_place|
+      if wish_place.place_id == place_id
+        value = true
+      end
+    end
+    return value
+  end
 end
